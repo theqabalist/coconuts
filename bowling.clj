9c4e73acf9c77e130afa437b513c8c6fc05c5841
Brandon Keown <keown.brandon@gmail.com>
2013-10-04 21:56:14 -0500

    Added solution for single strikes.

--- bowling.clj
+++ bowling.clj
@@ -1,14 +1,28 @@
 (defmulti calculate-special
     (fn [one two coll]
         (cond
+            (= 10 one) :strike
             (= 10 (+ one two)) :spare
             :else :regular)))
 (defmethod calculate-special :regular [one two coll] (+ one two))
 (defmethod calculate-special :spare [one two coll] (+ one two (first coll)))
+(defmethod calculate-special :strike [one two coll] (+ one two (first coll)))
 
-(defmulti calculate-score (fn [coll] coll))
-(defmethod calculate-score [] [coll] 0)
-(defmethod calculate-score :default [coll]
+(defmulti pins-in-next-frame (fn [coll] (= 10 (first coll))))
+(defmethod pins-in-next-frame true [coll] 10)
+(defmethod pins-in-next-frame false [coll] (+ (first coll) (-> coll rest first)))
+
+(defmulti calculate-score
+    (fn [coll]
+        (cond
+            (= 10 (first coll)) :strike
+            (= [] coll) :empty
+            :else :regular)))
+(defmethod calculate-score :empty [coll] 0)
+(defmethod calculate-score :strike [coll]
+    (let [tail (rest coll)]
+        (+ 10 (pins-in-next-frame tail) (calculate-score tail))))
+(defmethod calculate-score :regular [coll]
     (let [one (first coll)
           two (-> coll rest first)
           tail (-> coll rest rest)]
