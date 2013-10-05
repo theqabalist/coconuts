d6eee5b3d479f9549dd728445d213fd2553d4292
Brandon Keown <keown.brandon@gmail.com>
2013-10-04 21:37:56 -0500

    Added solution for calculating spares using multiple dispatch rules.

--- bowling.clj
+++ bowling.clj
@@ -1,4 +1,17 @@
-(defn calculate-score [coll]
-    (reduce + coll))
+(defmulti calculate-special
+    (fn [one two coll]
+        (cond
+            (= 10 (+ one two)) :spare
+            :else :regular)))
+(defmethod calculate-special :regular [one two coll] (+ one two))
+(defmethod calculate-special :spare [one two coll] (+ one two (first coll)))
+
+(defmulti calculate-score (fn [coll] coll))
+(defmethod calculate-score [] [coll] 0)
+(defmethod calculate-score :default [coll]
+    (let [one (first coll)
+          two (-> coll rest first)
+          tail (-> coll rest rest)]
+         (+ (calculate-special one two tail) (calculate-score tail))))
 
 (println (calculate-score (read-string (first *command-line-args*))))
