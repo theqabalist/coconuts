039ad4f21fac86c5a4a9d74b25cfa961ec1fb5d8
Brandon Keown <keown.brandon@gmail.com>
2013-10-04 22:18:52 -0500

    Changed tests to reflect real scoring rules rather than bad assumption.

--- bowling.clj
+++ bowling.clj
@@ -1,29 +1,27 @@
-(defmulti calculate-special
-    (fn [one two coll]
-        (cond
-            (= 10 (+ one two)) :spare
-            :else :regular)))
-(defmethod calculate-special :regular [one two coll] (+ one two))
-(defmethod calculate-special :spare [one two coll] (+ one two (first coll)))
-
-(defmulti pins-in-next-frame (fn [coll] (= 10 (first coll))))
-(defmethod pins-in-next-frame true [coll] 10)
-(defmethod pins-in-next-frame false [coll] (+ (first coll) (-> coll rest first)))
-
 (defmulti calculate-score
     (fn [coll]
-        (cond
-            (= 10 (first coll)) :strike
-            (= [] coll) :empty
-            :else :regular)))
+        (let [one (first coll)
+              two (-> coll rest first)
+              tail (-> coll rest rest)]
+              (cond
+                  (= [] coll) :empty
+                  (= 3 (count coll)) :irregular-tenth
+                  (= 10 one) :strike
+                  (= 10 (+ one two)) :spare
+                  :else :regular))))
+
 (defmethod calculate-score :empty [coll] 0)
+(defmethod calculate-score :irregular-tenth [coll] (reduce + coll))
 (defmethod calculate-score :strike [coll]
     (let [tail (rest coll)]
-        (+ 10 (pins-in-next-frame tail) (calculate-score tail))))
+        (+ 10 (first tail) (-> tail rest first) (calculate-score tail))))
+(defmethod calculate-score :spare [coll]
+    (let [tail (-> coll rest rest)]
+        (+ 10 (first tail) (calculate-score tail))))
 (defmethod calculate-score :regular [coll]
     (let [one (first coll)
           two (-> coll rest first)
           tail (-> coll rest rest)]
-         (+ (calculate-special one two tail) (calculate-score tail))))
+         (+ one two (calculate-score tail))))
 
 (println (calculate-score (read-string (first *command-line-args*))))
