44f70f742d01d2fce7215041eeddc35d67e17033
Brandon Keown <keown.brandon@gmail.com>
2013-10-09 21:20:16 -0500

    Added failing test plus some work for incremental game.

--- bowling.clj
+++ bowling.clj
@@ -5,14 +5,18 @@
               tail (-> coll rest rest)]
               (cond
                   (= [] coll) :empty
+                  (= 1 (count coll)) :singlet
                   (and (= 10 (+ one two)) (= [] tail)) :partial-spare
                   (= 3 (count coll)) :irregular-tenth
+                  (and (= 10 one) (= 0 (count tail))) :partial-strike
                   (= 10 one) :strike
                   (= 10 (+ one two)) :spare
                   :else :regular))))
 
 (defmethod calculate-score :empty [coll] 0)
+(defmethod calculate-score :singlet [coll] (first coll))
 (defmethod calculate-score :irregular-tenth [coll] (reduce + coll))
+(defmethod calculate-score :partial-strike [coll] (+ 10 (first coll)))
 (defmethod calculate-score :strike [coll]
     (let [tail (rest coll)]
         (+ 10 (first tail) (-> tail rest first) (calculate-score tail))))
@@ -26,4 +30,12 @@
           tail (-> coll rest rest)]
          (+ one two (calculate-score tail))))
 
-(println (calculate-score (read-string (first *command-line-args*))))
+(defn mainloop [scores]
+    (let [newscores (conj scores (-> (read-line) read-string))]
+        (println newscores)
+        (println (calculate-score newscores))
+        (recur newscores)))
+
+(if (nil? (first *command-line-args*))
+    (mainloop [])
+    (println (calculate-score (read-string (first *command-line-args*)))))
--- proc.py
+++ proc.py
@@ -5,3 +5,10 @@ def bowl(s):
     out, err = proc.communicate()
     assert err == ""
     return out.strip()
+
+def inc_bowl(l):
+    proc = subprocess.Popen(["clj","bowling.clj"],stdout=subprocess.PIPE,stderr=subprocess.PIPE,stdin=subprocess.PIPE)
+    input_ = "\n".join(l.split())
+    out, err = proc.communicate(input_)
+    assert err == ""
+    return out.strip()
